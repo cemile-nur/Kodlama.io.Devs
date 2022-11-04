fb8607d0a1702648ba77d974cd610a896e6c57c2
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2022-11-04 13:59:03 +0300

    started project

---
+++ kodlamaio_dev/src/main/java/com/example/kodlamaio_dev/KodlamaioDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodlamaio_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodlamaioDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodlamaioDevApplication.class, args);
+    }
+
+}
---
+++ kodlamaio_dev/src/test/java/com/example/kodlamaio_dev/KodlamaioDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodlamaio_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodlamaioDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
